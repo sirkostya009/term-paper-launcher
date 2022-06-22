72dcdc877e64d6df88c3637c83df6b4c0a01e108
Constantine <kosta.tovstik@gmail.com>
2022-06-23 02:27:19 +0300

    new version 0.5

--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,126 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::path::Path;
-use std::os::windows::process::CommandExt;
 use std::process::Command;
+use eframe::run_native;
+use egui::{Vec2, Visuals};
+use eframe::CreationContext;
+use egui::Button;
+use std::os::windows::process::CommandExt;
 use std::sync::mpsc::{Receiver, Sender};
-use eframe::{CreationContext, run_native};
-use egui::{Button, Color32, Vec2, Visuals};
+use std::thread::JoinHandle;
+use winapi::um::winbase::CREATE_NO_WINDOW;
 
-const GIT_LINK:&str   = "https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe";
-const JRE_LINK:&str   = "https://download.bell-sw.com/java/18.0.1.1+2/bellsoft-jre18.0.1.1+2-windows-amd64-full.msi";
-const VERSION:&str    = "18.0.1.1+2";
-const REPOSITORY:&str = "https://github.com/sirkostya009/java-sources";
-const DIRECTORY:&str  = "java-sources";
+const JRE_LINK:&str = "https://download.bell-sw.com/java/18.0.1.1+2/bellsoft-jre18.0.1.1+2-windows-amd64-full.msi";
+const SETUP:&str    = "bellsoft-jre18.0.1.1+2-windows-amd64-full.msi";
+const VERSION:&str  = "18.0.1.1+2";
+const GAEM_LINK:&str= "https://github.com/sirkostya009/term-paper/releases/download/second-release/term-paper.jar";
+const JAR_NAME:&str = "term-paper.jar";
+
+fn path() -> String {
+    format!("C:/Users/{}/AppData/Roaming/GaemApp", whoami::username())
+}
+
+fn jre_is_present(sender: Sender<String>) -> bool {
+    sender.send("checking if jre is present".to_string());
+    let result = Command::new("java")
+        .creation_flags(CREATE_NO_WINDOW)
+        .output()
+        .is_ok();
+
+    if !result {
+        result
+    } else {
+        String::from_utf8(
+            Command::new("java")
+                .creation_flags(CREATE_NO_WINDOW)
+                .arg("--version")
+                .output()
+                .unwrap().stdout)
+            .unwrap()
+            .contains(VERSION)
+    }
+}
+
+fn gaem_is_present(sender: Sender<String>) -> bool {
+    sender.send("checking if gaem is present".to_string());
+
+    if !std::path::Path::new(&format!("{}", path())).exists() {
+        std::fs::create_dir(path());
+    }
+
+    std::path::Path::new(&format!("{}/{JAR_NAME}", path())).exists()
+}
+
+fn curl(sender: Sender<String>, what: &'static str, link: &'static str) -> JoinHandle<()> {
+    std::thread::spawn(move || {
+        sender.send(format!("curling {what}..."));
+
+        Command::new("curl")
+            .current_dir(path())
+            .creation_flags(CREATE_NO_WINDOW)
+            .args(["-LO", link])
+            .output();
+
+        sender.send(format!("{what} has been succesffully curl'd"));
+    })
+}
+
+fn install_jre(sender: Sender<String>) {
+    std::thread::spawn(move || {
+        if curl(sender.clone(), "jre", JRE_LINK).join().is_err() {
+            sender.send("failed to curl jre".to_string());
+            return;
+        }
+
+        sender.send("starting installer...".to_string());
+        let ran = Command::new("msiexec")
+            .current_dir(path())
+            .creation_flags(CREATE_NO_WINDOW)
+            .args(["/i", SETUP])
+            .output()
+            .is_ok();
+
+        sender.send(
+            if ran {
+                "jre has been installed"
+            } else {
+                "failed to install jre"
+            }.to_string()
+        );
+    });
+}
+
+fn curl_gaem(sender: Sender<String>) {
+    curl(sender, "gaem", GAEM_LINK);
+}
 
 struct LauncherApp {
     status: String,
-    runnable: bool,
-    restart_required: bool,
-    receiver: Receiver<String>,
     sender: Sender<String>,
+    receiver: Receiver<String>,
 }
 
 impl LauncherApp {
-    fn new(cc: &CreationContext) -> LauncherApp {
+    pub fn new(cc: &CreationContext) -> Self {
         cc.egui_ctx.set_visuals(Visuals::dark());
 
         let (s, r) = std::sync::mpsc::channel();
 
-        let mut app = LauncherApp {
+        LauncherApp {
             status: "Standby".to_string(),
-            runnable: false,
-            restart_required: false,
+            sender: s,
             receiver: r,
-            sender: s
-        };
-        app.runnable = app.check_for_git() && app.check_for_java() && app.check_directory();
-
-        app
-    }
-
-    fn check_for(&mut self, what: &str) -> bool {
-        self.sender.send(format!("checking if {what} exists... ")).unwrap();
-
-        let result = !matches!(Command::new(what).creation_flags(0x08000000).output(), Err(_e));
-        self.sender.send(format!("{what} is {}available", if result { "" } else { "not " })).unwrap();
-
-        result
-    }
-
-    pub fn check_for_git(&mut self) -> bool {
-        self.check_for("git")
-    }
-
-    pub fn check_for_java(&mut self) -> bool {
-        let mut result = self.check_for("java");
-
-        if result {
-            let output = String::from_utf8(Command::new("java").arg("--version")
-                .creation_flags(0x08000000)
-                .output()
-                .unwrap()
-                .stdout)
-                .unwrap();
-
-            result &= output.contains(VERSION); // checks if exactly the same version of jre is installed,
-        }                                           // otherwise you'll need to install the one required
-
-        result
-    }
-
-    pub fn check_directory(&mut self) -> bool {
-        self.sender.send(String::from("checking if gaem directory exists...")).unwrap();
-
-        let mut result = true;
-
-        if !Path::new(DIRECTORY).exists() {
-            result = false;
         }
-
-        if !self.check_for_git() {
-            result = false;
-        }
-
-        self.sender.send((if result { "directory exists!" } else { "directory does not exist" }).to_string()).unwrap();
-        if !result { return result }
-
-        result = String::from_utf8(Command::new("git")
-            .creation_flags(0x08000000)
-            .current_dir(DIRECTORY)
-            .arg("status")
-            .output()
-            .unwrap().stdout)
-            .unwrap()
-            .contains(&"up to date");
-
-        self.sender.send(format!("directory is {}up to date", if result { "" } else { "not " })).unwrap();
-        if !result && Command::new("git")
-            .creation_flags(0x08000000)
-            .current_dir(DIRECTORY)
-            .arg("pull")
-            .output().is_ok()
-        {
-            result = true
-        }
-
-        result
     }
 
-    pub fn clone_repository(&mut self) {
-        self.sender.send(String::from("cloning gaem...")).unwrap();
-        std::thread::spawn(|| Command::new("git")
-            .creation_flags(0x08000000)
-            .args(["clone", REPOSITORY])
-            .output()
-            .expect("failed to clone from remote repository")
-        );
-    }
-
-    fn download_and_install(&mut self, what: &'static &str, link: &'static &str) {
-        let sender = self.sender.clone();
-
-        std::thread::spawn(move || {
-            sender.send(format!("downloading {what}")).unwrap();
-            Command::new("curl")
-                .creation_flags(0x08000000)
-                .args(["-O", link])
-                .output()
-                .unwrap_or_else(|err| {
-                    let msg = format!("failed to download {what}, {err}");
-                    sender.send(msg.clone()).unwrap();
-                    panic!("{msg}")
-                });
-
-            let setup = link.split('/').last().unwrap();
-            let extension = Path::new(setup).extension().unwrap();
-
-            sender.send(format!("installing {what}")).unwrap();
-            let handle = |err| {
-                let msg = format!("failed to install {what}, {err}");
-                sender.send(msg.clone()).unwrap();
-                panic!("{msg}")
-            };
-
-            if extension == "msi" {
-                Command::new("msiexec")
-                    .creation_flags(0x08000000)
-                    .args(["/i", setup])
-                    .output()
-                    .unwrap_or_else(handle);
-            } else if extension == "exe" {
-                Command::new(setup)
-                    .creation_flags(0x08000000)
-                    .arg("/VERYSILENT")
-                    .output()
-                    .unwrap_or_else(handle);
-            }
-        });
-
-        self.restart_required = true;
-    }
-
-    pub fn download_and_install_jre(&mut self) {
-        self.download_and_install(&"jre",&JRE_LINK);
-    }
-
-    pub fn download_and_install_git(&mut self) {
-        self.download_and_install(&"git",&GIT_LINK);
-    }
-
-    fn poll_status(&mut self) {
+    pub fn poll_status(&mut self) {
         if let Ok(s) = self.receiver.try_recv() {
             self.status = s;
         }
+    }
+
+    pub fn sender(&self) -> Sender<String> {
+        self.sender.clone()
     }
 }
 
@@ -182,23 +131,24 @@ impl eframe::App for LauncherApp {
 
             ui.vertical_centered(|ui| {
                 if ui.add_sized(Vec2::new(160f32,50f32),Button::new("Run")).clicked() {
-                    if self.runnable {
-                        std::thread::spawn(||Command::new("java")
-                            .current_dir(format!("./{DIRECTORY}/"))
-                            .creation_flags(0x08000000)
-                            .arg("Main")
-                            .output()
-                            .expect("java Main failed"));
+                    if !jre_is_present(self.sender()) {
+                        install_jre(self.sender());
+                    } else if !gaem_is_present(self.sender()) {
+                        curl_gaem(self.sender());
                     } else {
-                        if !self.check_for_git() {
-                            self.download_and_install_git();
-                        } else if !self.check_for_java() {
-                            self.download_and_install_jre();
-                        } else if !self.check_directory() {
-                            self.clone_repository();
-                        }
-
-                        self.runnable = self.check_for_git() && self.check_for_java() && self.check_directory();
+                        let sender = self.sender();
+                        sender.send("starting gaem...".to_string());
+                        std::thread::spawn(move ||
+                            Command::new("java")
+                                .current_dir(path())
+                                .args(["-jar", JAR_NAME])
+                                .output()
+                                .unwrap_or_else(|err| {
+                                    let msg = format!("failed to launch gaem, {err}");
+                                    sender.send(msg.clone());
+                                    panic!("{msg}")
+                                })
+                        );
                     }
                 }
 
@@ -207,12 +157,6 @@ impl eframe::App for LauncherApp {
                 ui.add_space(10f32);
 
                 ui.label(self.status.as_str());
-
-                if self.restart_required {
-                    ui.label(egui::RichText::new("program restart required").color(Color32::RED));
-                } else if self.runnable {
-                    self.sender.send("Ready".to_string()).unwrap();
-                }
             })
         });
     }
